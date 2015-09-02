69e79764d78722a4f6e4496a0b6f0d5870ea51d7
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2015-09-02 15:25:17 +0300

    custom count

--- app/models/record.rb
+++ app/models/record.rb
@@ -146,7 +146,7 @@ class Record < ActiveRecord::Base
 
   def self.search(params)
     p params
-    n = params[:n] || 1000
+    n = params[:n] || 10000
     records = Record.limit(n) #if params.
 
     sym = params[:client_ip]
@@ -196,7 +196,7 @@ class Record < ActiveRecord::Base
     # records = records.where(url: sym) if sym.present?
     records = records.where('url similar to ?', sym) if sym.present?
 
-    records.limit(1000)#.order(session_start: :desc)
+    records.order(session_start: :desc)
   end
 
   def self.size
